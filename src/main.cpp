0080ab23216bd16da3510153834ab7cbf763042e
Scott Loughmiller <loughmsa@gmail.com>
2019-07-21 10:57:53 -0700

    port changes from staff, remove dependency on FastLEDVisualizations lib

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <arm_math.h>
 #include <FastLED.h>
 #include <algorithm>    // std::sort
-#include <Visualization.h>
 
 using namespace std; 
 
@@ -13,6 +12,10 @@ using namespace std;
 const int numLEDs{24};               // MEDALLION
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+// NOTE DETECTION
+////////////////////////////////////////////////////////////////////////////////////////////////
+
 // NOTE DETECTION CONSTANTS
 const uint_fast16_t fftSize{256};               // Size of the FFT.  Realistically can only be at most 256
 const uint_fast16_t fftBinSize{8};              // Hz per FFT bin  -  sample rate is fftSize * fftBinSize
@@ -21,25 +24,31 @@ const uint_fast16_t sampleCount{fftSize * 2};   // Complex FFT functions require
 const float middleA{440.0};                     // frequency of middle A.  Needed for freqeuncy to note conversion
 const uint_fast16_t sampleIntervalMs{1000000 / (fftSize * fftBinSize)};  // how often to get a sample, needed for IntervalTimer
 
+// FREQUENCY TO NOTE CONSTANTS - CALCULATE HERE: https://docs.google.com/spreadsheets/d/1CPcxGFB7Lm6xJ8CePfCF0qXQEZuhQ-nI1TC4PAiAd80/edit?usp=sharing
+const uint_fast16_t noteCount{41};              // how many notes are we trying to detect
+const uint_fast16_t notesBelowMiddleA{30};      
+
 // NOTE DETECTION GLOBALS
 float samples[sampleCount*2];
 uint_fast16_t sampleCounter = 0;
 float sampleBuffer[sampleCount];
 float magnitudes[fftSize];
+float noteMagnatudes[noteCount];
 arm_cfft_radix4_instance_f32 fft_inst;
 IntervalTimer samplingTimer;
 
 // NOTE DETECTION FUNCTIONS
 void noteDetectionSetup();        // run this once during setup
 void noteDetectionLoop();         // run this once per loop
-void samplingCallback();    
+void samplingCallback();
 
-float noteMagnatudes[numLEDs];
+////////////////////////////////////////////////////////////////////////////////////////////////
+// \ NOTE DETECTION
+////////////////////////////////////////////////////////////////////////////////////////////////
+
 CRGB leds[numLEDs];
 const int saturation{244};
-
 int hue = 0;
-Visualization * all;
 
 void setup() {
   delay(3000);
@@ -51,11 +60,6 @@ void setup() {
 
   // DISPLAY STUFF
   FastLED.addLeds<NEOPIXEL, DISPLAY_LED_PIN>(leds, numLEDs).setCorrection( TypicalLEDStrip );;
-  all = new Visualization(numLEDs, 1, hue, saturation, leds);
-  FastLED.setBrightness(32);
-  // FastLED.setDither(0); 
-  all->setAllCRGB(0x101010);
-  FastLED.show();
   delay(1000);
 
   Serial.println("setup complete");
@@ -63,46 +67,24 @@ void setup() {
 
 int time = -1;
 int iteration = 0;
-float threshold = 1000.0;
+float threshold = 500.0;
 float peak = 2000.0;
 
 void loop() {
-  // Serial.println("Start Loop");
-  all->setAllCRGB(0x000000);  // this only clears the array, not the LEDs, it's fine at the top
-
-  hue+=3;
-  all->setHue(hue);
-
   bool newSec = false;
-
+  hue+=3;
   iteration++;
 
   noteDetectionLoop();
 
-  for (uint_fast16_t i=0; i<numLEDs; i++) {
-    noteMagnatudes[i] = 0;
-  }
-
-  for (uint_fast16_t i=1; i<fftSize/2; i++) {  // ignore top half of the FFT results
-    float frequency = i * (fftBinSize);
-    int note = roundf(12 * (log(frequency / middleA) / log(2))) + 32;
-
-    if (note < 0) {
-      continue;
-    }
-
-    note = note % numLEDs;
-    noteMagnatudes[note] = max(noteMagnatudes[note], magnitudes[i]);
-  }
-
-  float sorted[numLEDs];
-  memcpy(sorted, noteMagnatudes, sizeof(noteMagnatudes[0]) * numLEDs);
+  float sorted[noteCount];
+  memcpy(sorted, noteMagnatudes, sizeof(noteMagnatudes[0]) * noteCount);
   sort(sorted, sorted+sizeof(sorted)/sizeof(sorted[0]));
 
-  float twoThirds = sorted[18];
-  float maxNote = sorted[22];
-  threshold = (threshold * (99.0/100.0)) + (twoThirds/100.0);
-  peak = (peak * (199.0/200.0)) + (maxNote/200.0);
+  float twoThirds = sorted[(uint_fast16_t) (0.75 * noteCount)];
+  float maxNote = sorted[noteCount - 1];
+  threshold = (threshold * (0.99)) + (twoThirds/100.0);
+  peak = (peak * (0.99)) + (maxNote/100.0);
 
   if (newSec) {
     Serial.println(twoThirds);
@@ -113,7 +95,7 @@ void loop() {
     Serial.println(peak);
   }
 
-  for (uint_fast16_t i=0; i<numLEDs; i++) {
+  for (uint_fast16_t i=0; i<noteCount; i++) {
     if (noteMagnatudes[i] >= threshold) {
       int value = 0;
       if (noteMagnatudes[i] > peak) {
@@ -121,13 +103,19 @@ void loop() {
       } else {
         value = ((noteMagnatudes[i] - threshold) / (peak - threshold)) * 255;
       }
-      all->setValue(value);
-      all->setLEDColor(i);
+
+      leds[i % 24] = CHSV(hue, saturation, value);
+    } else {
+      leds[i % 24] = CRGB::Black;
     }
   }
 
    FastLED.show(); 
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// NOTE DETECTION
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 void noteDetectionSetup() {
   pinMode(AUDIO_INPUT_PIN, INPUT);
@@ -144,6 +132,22 @@ void noteDetectionLoop() {
   // FFT magic
   arm_cfft_radix4_f32(&fft_inst, sampleBuffer);
   arm_cmplx_mag_f32(sampleBuffer, magnitudes, fftSize);
+
+  for (uint_fast16_t i=0; i<noteCount; i++) {
+    noteMagnatudes[i] = 0;
+  }
+
+  for (uint_fast16_t i=1; i<fftSize/2; i++) {  // ignore top half of the FFT results
+    float frequency = i * (fftBinSize);
+    int note = roundf(12 * (log(frequency / middleA) / log(2))) + notesBelowMiddleA;
+
+    if (note < 0) {
+      continue;
+    }
+
+    note = note % noteCount;
+    noteMagnatudes[note] = max(noteMagnatudes[note], magnitudes[i]);
+  }  
 }
 
 void samplingCallback() {
@@ -165,3 +169,7 @@ void samplingCallback() {
   sampleCounter++;
   sampleCounter = sampleCounter % fftSize;
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// \ NOTE DETECTION
+////////////////////////////////////////////////////////////////////////////////////////////////
